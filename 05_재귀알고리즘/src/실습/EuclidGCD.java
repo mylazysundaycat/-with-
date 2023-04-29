02b86c86dad8e8331111af541920748631a13ec2
mylazysundaycat <mylazysundaycat@gmail.com>
2023-04-29 22:11:32 +0900

    2023-04-29<162p-170p>

---
+++ 05_재귀알고리즘/src/실습/EuclidGCD.java
@@ -0,0 +1,25 @@
+package 실습;
+
+public class EuclidGCD {
+	public static int euclidGCD(int a, int b) {
+			int maxNum = Math.max(a, b);
+			int minNum = Math.min(a, b);
+			
+			int q = maxNum/minNum;
+			int r = maxNum%minNum;
+			
+			if(r==0) {
+				return minNum;
+			} else {
+				return euclidGCD(r,b);
+			}
+	}
+	
+	public static void main(String[] args) {
+		int a = 50;
+		int b = 15;
+		
+		int c = euclidGCD(a,b);
+		System.out.println(c);
+	}
+}
